5c019009c922b21fa08ee1340c7c923d8545887b
sugar_tree <sugar_tree@outlook.kr>
2024-03-02 14:43:53 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+document.body.addEventListener("mousemove", (evt) => {
+	const mouseX = evt.clientX;
+	const mouseY = evt.clientY;
+
+	gsap.set(".cursor", {
+		x: mouseX,
+		y: mouseY,
+	});
+});
+
+window.addEventListener("keydown", (text) => {
+	let t = text.key.toUpperCase();
+	let k = document.querySelector(`#${t}`);
+    if(k) {
+        k.classList.add("key-pressed");
+    }
+});
+
+window.addEventListener("keyup", (text) => {
+	let t = text.key.toUpperCase();
+	let k = document.querySelector(`#${t}`);
+    if(k) {
+        k.classList.remove("key-pressed");
+    }
+});
+
