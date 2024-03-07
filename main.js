1768c0aa0b240359563dd9ad853d76c2db9a0bee
eaea54 <a01020577855@gmail.com>
2024-03-08 02:57:45 +0900

    .

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var pressed = false;
-const apply_button = document.getElementById("apply-button");
+const apply_button = document.getElementById('apply-button');
 
-apply_button.addEventListener("click", function (event) {
-    if (!pressed) {
-        event.preventDefault();
-        apply_button.innerText = "신청 바로가기 (아래 내용 꼭 읽어주세요!)\n(한 번 더 누르면 내려갑니다)"
-        pressed = true;
-    }
-})
+apply_button.addEventListener('click', function (event) {
+  if (!pressed) {
+    event.preventDefault();
+    apply_button.innerText =
+      '신청 바로가기 \n(설마 밑에 안 읽으신건 아니죠..?)';
+    pressed = true;
+  }
+});
