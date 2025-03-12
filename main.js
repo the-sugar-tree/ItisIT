5012639802c3b4dfa9fae02eb31c8ef63637a0b1
gmts117 <chgpig117@gmail.com>
2025-03-12 13:52:42 +0900

    신청 링크 수정

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ function setupButtonEvents() {
   if (mainApplyButton) {
     mainApplyButton.addEventListener('click', function(event) {
       event.preventDefault();
-      const applicationUrl = "https://www.youtube.com/channel/UC8qO5racajmy4YgPgNJkVXg";
+      const applicationUrl = "https://forms.gle/UgucYhfGQ23WXC1V8";
       
       showCustomAlert("반드시 학교계정으로 로그인 해 주세요!", function() {
         window.open(applicationUrl, '_blank');
